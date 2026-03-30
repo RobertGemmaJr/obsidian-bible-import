00f1a2d258278a3eebbcfb7408d8506126c6ba83
RobertGemmaJr <rgemma.jr@gmail.com>
2026-03-29 20:13:27 -0400

    chore: Init files

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .constants import ROOT_PATH, DATA_PATH, SUPPORTED_TRANSLATIONS
+from .readers import create_bible_books
+
+__all__ = ["ROOT_PATH", "DATA_PATH", "SUPPORTED_TRANSLATIONS", "create_bible_books"]
