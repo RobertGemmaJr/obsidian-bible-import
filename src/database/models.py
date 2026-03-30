bdde2ea7eea8d978a761d8e858c9b204b888f742
RobertGemmaJr <rgemma.jr@gmail.com>
2026-03-29 21:15:54 -0400

    wip: add verse model

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,16 @@
 import uuid
 import json
 
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional
+
+import enum
+
+
+class Translation(str, enum.Enum):
+    """Supported bible translation"""
+
+    ESV = "ESV"
 
 
 class Book(SQLModel, table=True):
@@ -13,6 +22,11 @@ class Book(SQLModel, table=True):
     name: str = Field(index=True)
     short_name: str = Field(index=True)
     num_chapters: int
+
+    # Relations
+    verses: list["Verse"] = Relationship(back_populates="book")
+
+    # JSON
     matching_names: str = Field(default="[]")
 
     def get_matching_names(self) -> list[str]:
@@ -22,3 +36,16 @@ class Book(SQLModel, table=True):
     def set_matching_names(self, names: list[str]):
         """Set matching names from a list."""
         self.matching_names = json.dumps(names)
+
+
+class Verse:
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    translation: Translation
+    chapter_num: int
+    verse_num: int
+    text: str = Field(index=True)
+    comment: Optional[int]
+
+    # Relations
+    book_id: int = Field(default=None, foreign_key="book.id")
+    book: Book = Relationship(back_populates="verses")
